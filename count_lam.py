cfc3d158df38b41223b05dcbd27d2f53d96b3c91
moyo <moyo3ib@gmail.com>
2020-03-10 23:24:05 +0100

    update

--- count_lam.py
+++ count_lam.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import sys
 
 def sum(T,i,k):
     """Fonction qui somme les entiers du Tableau T
@@ -21,8 +22,8 @@ def sum_profile(T_1,T_2):
     """Fait la somme par indice commun des elements des 2 tableaux
     
     Arguments:
-        T_1 {array} -- Le tableau representant le pool profile 1
-        T_2 {array} -- Le tableau representant le pool profile 2
+        T_1 {array} -- Le tuple representant le pool profile 1
+        T_2 {array} -- Le tuple representant le pool profile 2
 
     Returns:
         array -- Le resultat de la somme des pool profile dont la longueur max des 2
@@ -31,12 +32,11 @@ def sum_profile(T_1,T_2):
     p2 = len(T_2)
     Tr = [0]*(max(p1,p2))
 
-   
     if p1 == 0 :
-        return T_2
+        return tuple(T_2)
     
     if p2 == 0:
-        return T_1
+        return tuple(T_1)
 
     for i in range(min(p1,p2)):
         Tr[i] = T_1[i]+T_2[i]
@@ -46,15 +46,7 @@ def sum_profile(T_1,T_2):
     elif(p2 > p1):
         for i in range(p1,p2):
             Tr[i] = T_2[i]
-
-    print(T_1)
-    print(T_2)
-    print("som")
-    print(Tr)
-
-    return Tr
-
-
+    return tuple(Tr)
 
 def e_k(k):
     """ Un profile avec k element e^k(n) = (0........1) dont len(0........1) = n
@@ -67,7 +59,24 @@ def e_k(k):
     """
     l    = [0]*(k+1)
     l[k] = 1
-    return l
+    return tuple(l)
+    
+#On unie deux dictionnaires si une cle se retrouve dans les deux dictionnaires on fait la somme de leurs elements
+def union_dic(dic1, dic2):
+  dicr={}
+  for key in dic1:
+    if key in dic2:
+      dicr[key]=dic1[key]+dic2[key]
+    else:
+      dicr[key]=dic1[key]
+  for key in dic2:
+    if not key in dic1:
+      dicr[key]=dic2[key]
+      
+  
+  return dicr
+      
+      
 
 
 def count (n,profile,s):
@@ -84,35 +93,40 @@ def count (n,profile,s):
     d = {}
     k = len(profile)
 
+    """On a effectué une legere modification sur l'algorithme dans le cas ou n==0 on ne prend pas en compte cet arbre
+        et dans le cas ou n==1 on calcul normalement"""
+
     if n == 1 :
         som = sum(profile,0,k-1)
-        S = (som*(som-1)) - s
+        S = som*(som-1)-s
         if S > 0:
-            d = { tuple(e_k(k)):S}
+            d = {e_k(k):S}
     else:
         for i in range(n):
             d_0 = {}
             if i == 0:
-                d_0 = {():sum(profile,0,k-1)}
+              d_0 = {():sum(profile,0,k-1)}
             else:
                 for k_0 in range(1,k):
-                    d_0.update( count(i, profile[:k_0], profile[k_0]) )
+                    d_0 = union_dic(d_0, count(i, profile[:k_0], profile[k_0]) )
             for (l,w_0) in d_0.items():
                 d_1 = {}
-                pp  = sum_profile(profile,list(l))
+                pp  = sum_profile(profile,l)
                 if n-1-i == 0:
-                    d_1.update({():(-1 + sum(pp,0,k-1))})
+                    d_1 = union_dic(d_1,{():(-1 + sum(pp,0,k-1))})
                 else:
                     for k_1 in range(1,k):
-                        d_1.update(count(n-1-i,pp[:k_1],pp[k_1]))
+                        d_1 = union_dic(d_1,(count(n-1-i,pp[:k_1],pp[k_1])))
                 for (r,w_1) in d_1.items():
                     w = w_0*w_1
-                    t = sum_profile(sum_profile(l,list(r)), e_k(k))
-                    if tuple(t) in d.keys():
-                        d[tuple(t)] = d[tuple(t)] + w
+                    t = sum_profile( sum_profile(l,r), e_k(k))
+                    if t in d:
+                        d[t] = d[t] + w
                     else:
-                        d.update({tuple(t):w})
+                        d[t]=w
     return d
 
-
-print(count(3,[2,0,0],0))
+if len(sys.argv)<3:
+    print("Le format est python3 count.py [nombre de noeud de la bdd] [nombre de variables]")
+else:
+  print(count(int(sys.argv[1]),tuple([2]+[0]*(int(sys.argv[2])-1)),0))
